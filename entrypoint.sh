47337c8183a6f5a7edbe80c886c09576a35c0961
Nanne <nanne@mycel.nl>
2019-04-06 14:36:02 +0200

    Add private key for git authentication

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@ set -e
 echo "#################################################"
 echo "Starting the git Action"
 
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_rsa
+
 sh -c "$*"
 
 echo "#################################################"
