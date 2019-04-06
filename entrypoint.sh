bb9f516139ff537db51dd37c64b7d6bd05f9c8c8
Nanne <nanne@mycel.nl>
2019-04-06 14:38:16 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 echo "#################################################"
 echo "Starting the git Action"
 
+mkdir ~/.ssh
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
 eval $(ssh-agent -s)
